ea58dc3401ec8cb10a4570d9e1af12f566d1c7b7
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-07-29 08:58:42 +0530

    Create Color_Pair_To_String.c

---
+++ Color_Pair_To_String.c
@@ -0,0 +1,5 @@
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
