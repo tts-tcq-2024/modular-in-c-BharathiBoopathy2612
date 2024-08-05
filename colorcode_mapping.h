de972ab2619cf668ecce532a20e664bd539c3268
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-05 10:58:48 +0530

    Delete colorcode_mapping.h

--- colorcode_mapping.h
+++
@@ -1,9 +0,0 @@
-#ifndef COLOR_MAPPING_H
-#define COLOR_MAPPING_H
-
-#include "colorcode_mapping.h"
-
-ColorPair GetColorFromPairNumber(int pairNumber);
-int GetPairNumberFromColor(const ColorPair* colorPair);
-
-#endif
