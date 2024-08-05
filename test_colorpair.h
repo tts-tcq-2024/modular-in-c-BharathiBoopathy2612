35638398faa96c72057138258b208fb26a5bb78f
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-05 10:59:32 +0530

    Delete test_colorpair.h

--- test_colorpair.h
+++
@@ -1,7 +0,0 @@
-#ifndef TEST_COLORPAIR_H
-#define TEST_COLORPAIR_H
-
-void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
-void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
-
-#endif // TEST_COLORPAIR_H
