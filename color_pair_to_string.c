76808d16bcbb111070b385c09478ee8b770298cb
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-05 10:58:26 +0530

    Delete color_pair_to_string.c

--- color_pair_to_string.c
+++
@@ -1,8 +0,0 @@
-#ifndef COLORPAIR_TO_STRING_H
-#define COLORPAIR_TO_STRING_H
-
-#include "colorpair.h"
-
-void ColorPairToString(const ColorPair* colorPair, char* buffer);
-
-#endif // COLORPAIR_TO_STRING_H
