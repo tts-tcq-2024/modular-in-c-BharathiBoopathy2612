a971f0f969f8b893a6292f3083e8a0335b4bedb4
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-05 10:57:47 +0530

    Delete Get_Color_From_Pair_Number.c

--- Get_Color_From_Pair_Number.c
+++
@@ -1,9 +0,0 @@
-ColorPair GetColorFromPairNumber(int pairNumber) {
-    ColorPair colorPair;
-    int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = 
-        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    colorPair.minorColor =
-        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-    return colorPair;
-}
